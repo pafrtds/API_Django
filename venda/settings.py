735a890222360f1890b8d34da631b038967b1122
pafrtds <32377617+pafrtds@users.noreply.github.com>
2019-10-14 23:00:47 -0300

    Update settings.py

--- venda/settings.py
+++ venda/settings.py
@@ -112,6 +112,7 @@ DEFAULT_CHARSET = 'utf-8'
 #	#'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
